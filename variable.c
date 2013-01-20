b92b99760fdf941d8dec39c505fa7b331ba8fac9
Ludovic Hofer <medrimonia@gmail.com>
2013-01-20 17:28:45 +0100

    Adding a copy_variable and a destroy_variable function

--- variable.c
+++ variable.c
@@ -8,3 +8,24 @@ variable_p new_variable(linked_list_pointer allowed_types, char * name){
 	new->name = name;
 	return new;
 }
+
+variable_p copy_variable(variable_p v){
+	linked_list_pointer types_copy = new_linked_list();
+	// copying types
+	if (linked_list_size(v->allowed_types) != 0){
+		linked_list_restart(v->allowed_types);
+		while(true){
+			linked_list_append(types_copy,linked_list_get(v->allowed_types));
+			if (linked_list_end(v->allowed_types))
+				break;
+			linked_list_next(v->allowed_types);
+		}
+	}
+	// types copy ok
+	return new_variable(types_copy, v->name);	
+}
+
+void destroy_variable(variable_p v){
+	linked_list_destroy_opt_erase(v->allowed_types,false);
+	free(v);
+}
--- variable.h
+++ variable.h
@@ -15,4 +15,12 @@ typedef struct variable * variable_p;
  */
 variable_p new_variable(linked_list_pointer allowed_types, char * name);
 
+/* Copy the specified variable, the list is copied, but the name pointer is
+ * keeped.
+ */
+variable_p copy_variable(variable_p v);
+
+/* Destroy the specified variable, free allowed_types but not name */
+void destroy_variable(variable_p v);
+
 #endif
