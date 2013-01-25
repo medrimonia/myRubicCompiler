20fdf4c42683715a5fdb2deb700c53e78ad7f5ad
Ludovic Hofer <medrimonia@gmail.com>
2013-01-26 00:16:03 +0100

    Increasing free from 3332/3442 to 3344/3442 for ./rubic <tests/test_appel.rubicg

--- tree.c
+++ tree.c
@@ -74,6 +74,10 @@ void destroy_call(tn_pointer node){
 void destroy_tree(tn_pointer node){
 	if (node == NULL)
 		return;
+	if (node->allowed_types != NULL ||
+			node->type == IDENTIFIER    ||
+			node->type == AFFECT)
+		linked_list_destroy_opt_erase(node->allowed_types, false);
 	destroy_tree(node->left_child);
 	destroy_tree(node->right_child);
 	switch(node->type){
