7587f0f59f002c0162c6e17bb25f42e793118c29
Ludovic Hofer <medrimonia@gmail.com>
2013-01-26 00:57:12 +0100

    Increasing free from 3401/3442 to 3407/3442 for ./rubic <tests/test_appel.rubic

--- function.c
+++ function.c
@@ -157,6 +157,7 @@ bool is_built_in(function_p f){
 void function_destroy(function_p f){
 	linked_list_destroy_opt_erase(f->valid_prototypes, false);
 	linked_list_destroy_opt_erase(f->parameters, false);
+	linked_list_destroy_opt_erase(f->possible_return_types, false);
 	free(f);
 }
 
